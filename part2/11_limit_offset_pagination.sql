510e6fd4f5756d7b3a507f5e1fef0937226ec6bc
Shaahzad <shehzadmemon147@gmail.com>
2026-06-28 17:19:13 +0500

    learn Advance postgreSQL part 2

---
+++ part2/11_limit_offset_pagination.sql
@@ -0,0 +1,16 @@
+
+
+
+--  limit How Many Rows You Have To Return 
+--  Offset How Many Rows We Want To Skip
+
+-- SELECT name, price
+-- FROM products
+-- ORDER BY name ASC
+-- LIMIT 5 OFFSET 0;
+
+
+-- SELECT name, price
+-- FROM products
+-- ORDER BY name ASC
+-- LIMIT 5 OFFSET 5;

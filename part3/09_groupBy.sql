b8cb8b2cce03ac80025859e4276f25407685a70e
Shaahzad <shehzadmemon147@gmail.com>
2026-07-03 00:51:50 +0500

    learn Advance postgreSQL part 3

--- part3/09_groupBy.sql
+++ part3/09_groupBy.sql
@@ -5,3 +5,13 @@
 
 -- find author who have written atleast 2 post
 
+SELECT 
+u.name AS author_name,
+COUNT(p.id) AS total_post,
+SUM(p.views) AS total_views
+FROM users AS u 
+LEFT JOIN posts AS p 
+  ON u.id = p.user_id
+GROUP BY u.id, u.name
+HAVING COUNT(p.id) = 1
+ORDER BY total_post DESC;
